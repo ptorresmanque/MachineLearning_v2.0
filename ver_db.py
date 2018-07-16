cc68df9260d81e7455cc0e78e03d1f6ce81d6ade
Benjamin Navarrete <bnavarrete2012@alu.uct.cl>
2018-07-16 16:29:36 -0400

    asd

--- ver_db.py
+++ ver_db.py
@@ -3,8 +3,9 @@ import sqlite3
 conn = sqlite3.connect('ej.db')
 
 c = conn.cursor()
-
-for row in c.execute('SELECT * FROM features'): # mostrar base de datos
-    print row
-
+f = open("data.txt","w")
+for row in c.execute('SELECT ancho, alto, clase FROM features'): # mostrar base de datos
+    print(row)
+    f.write(' '.join(str(s) for s in row) + '\n')
 conn.close()
+f.close()
