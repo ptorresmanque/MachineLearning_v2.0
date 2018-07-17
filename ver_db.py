c716a85845a0351ec2d7b50414879cf53fe890c5
Benjamin Navarrete <bnavarrete2012@alu.uct.cl>
2018-07-16 22:55:34 -0400

    1234

--- ver_db.py
+++ ver_db.py
@@ -3,9 +3,9 @@ import sqlite3
 conn = sqlite3.connect('ej.db')
 
 c = conn.cursor()
-f = open("data.txt","w")
+f = open("data.txt","w") ##crear archivo de texto para el regresor logístico
 for row in c.execute('SELECT ancho, alto, clase FROM features'): # mostrar base de datos
     print(row)
-    f.write(' '.join(str(s) for s in row) + '\n')
+    f.write(' '.join(str(s) for s in row) + '\n') #escribimos el archivo de texto con los datos de la bd
 conn.close()
 f.close()
